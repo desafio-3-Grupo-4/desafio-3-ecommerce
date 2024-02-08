30eae274773c203ae164012bcabc7675071ca8ba
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-08 19:56:04 -0300

    created-notFoundException-teste

--- ecommerce-engrenagem/src/main/java/application/exceptions/NotFoundException.java
+++ ecommerce-engrenagem/src/main/java/application/exceptions/NotFoundException.java
@@ -3,5 +3,6 @@ package application.exceptions;
 public class NotFoundException extends RuntimeException{
     public NotFoundException(String message) {
         super(message);
+        System.out.println("Teste");
     }
 }
