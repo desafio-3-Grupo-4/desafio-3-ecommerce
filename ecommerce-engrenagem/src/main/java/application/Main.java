6864980ae5a9bef567006e006c5f36d29c05c0d7
Igor Farias <balestfarias@gmail.com>
2024-02-08 15:32:00 -0300

    :sparkles: product-dao

--- ecommerce-engrenagem/src/main/java/application/Main.java
+++ ecommerce-engrenagem/src/main/java/application/Main.java
@@ -16,16 +16,6 @@ public class Main {
         Product p2 = new Product( null, "wolfstein", "Kill Nazis", 20.0);
         Product p3 = new Product( null, "resident evil 4 remake", "kill las plaguas", 50.0);
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ecommerce-jpa");
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-
-        em.getTransaction().commit();
 
         System.out.println(p1);
         System.out.println(p2);
